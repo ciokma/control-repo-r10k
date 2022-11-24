9e950dce9267454db6ea036c89de53e0fdd9aa16
Ubuntu <ubuntu@ip-172-31-4-237.ec2.internal>
2022-11-24 21:14:41 +0000

    adding hostname in apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,7 +3,7 @@ class profile::apache {
     serveradmin =>'maurez89@yahoo.es',
   }
   
-  apache::vhost {'vhost.example.com':
+  apache::vhost {'ip-172-31-6-47.ec2.internal':
     port => '80',
     docroot => '/var/www/vhost',
   }
