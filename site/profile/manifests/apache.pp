df77527cc21bea33970aea2fe507e0fb5797adda
Ubuntu <ubuntu@ip-172-31-4-237.ec2.internal>
2022-11-24 21:16:07 +0000

    commenting

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,9 +3,9 @@ class profile::apache {
     serveradmin =>'maurez89@yahoo.es',
   }
   
-  apache::vhost {'ip-172-31-6-47.ec2.internal':
-    port => '80',
-    docroot => '/var/www/vhost',
-  }
+#  apache::vhost {'ip-172-31-6-47.ec2.internal':
+#    port => '80',
+#    docroot => '/var/www/vhost',
+#  }
 
 }
