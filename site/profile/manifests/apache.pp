1e58aeffa674bb14e680208ce441c24bc2a7e0c5
ciokma <53948541+ciokma@users.noreply.github.com>
2022-11-25 08:29:16 -0600

    commenting apache code

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,7 +1,7 @@
 class profile::apache {
-  class {'::apache':
-    serveradmin =>'maurez89@yahoo.es',
-  }
+#  class {'::apache':
+#    serveradmin =>'maurez89@yahoo.es',
+#  }
   
 #  apache::vhost {'ip-172-31-6-47.ec2.internal':
 #    port => '80',
