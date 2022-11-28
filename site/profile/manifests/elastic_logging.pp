970d2daf7e8296d1c04fc13f49941c815f38b581
ciokma <53948541+ciokma@users.noreply.github.com>
2022-11-28 07:42:54 -0600

    f

--- site/profile/manifests/elastic_logging.pp
+++ site/profile/manifests/elastic_logging.pp
@@ -4,11 +4,5 @@ class profile::elastic_logging
   class { 'elastic_stack::repo':
     version => 8,
   }
-  include ::java
-
-  class { 'elasticsearch':
-    version => 8.5.2,
-    status => 'enabled',
-  }
 
 }
