981316a6deb110b55356ca387003dca47ea1a7ca
ciokma <53948541+ciokma@users.noreply.github.com>
2022-11-28 10:14:17 -0600

    f

--- site/profile/manifests/elastic_logging.pp
+++ site/profile/manifests/elastic_logging.pp
@@ -4,8 +4,9 @@ class profile::elastic_logging
   include ::java
 
   class { 'elasticsearch': 
-     version => '8.0.2',
+     version => '7.9.3',
      ensure => 'present',
+     status => 'running'
      jvm_options => [
         '-Xms1g',
         '-Xmx1g'
